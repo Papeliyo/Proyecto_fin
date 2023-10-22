d6a642594e0743fdfea377989f21e47bb86c51a6
Papeliyo <>
2023-10-22 15:18:29 -0300

    Unit test 90%

--- proyecto_final/test_task_connect.py
+++ proyecto_final/test_task_connect.py
@@ -1,13 +1,32 @@
 from datetime import date
-from main import insert, update, delete
+from main import insert, update, delete, update_state
 from schema.task_schema import TaskSchema
+from fastapi.testclient import TestClient
+from main import app
+import pytest
+
+@pytest.fixture
+def client():
+    # Create a test client for FastAPI app
+    client = TestClient(app)
+    return client
+
+def test_root(client):
+    # Call the root endpoint
+    response = client.get("/api/view")
+
+    # Check if the response status code is 200 (OK)
+    assert response.status_code == 200
+
+    # Check if the response is a JSON list
+    assert isinstance(response.json(), list)
 
 # inserts a new task with valid data
 def test_insert_valid_data():
 
     # Create a valid task data as a TaskSchema object
     task_data = TaskSchema(
-        titulo="Task 4",
+        titulo="titulo5",
         descripcion="valid data",
         fecha_vencimiento=date.today(),
         estado="pendiente"
@@ -21,7 +40,7 @@ def test_update_valid_data():
 
     # Create a mock task_data object
     task_data = TaskSchema(
-        titulo="Task 4",
+        titulo="titulo5",
         descripcion="dato para borrar",
         fecha_vencimiento="2023-10-10",
         estado="Completado"
@@ -30,9 +49,22 @@ def test_update_valid_data():
     # Call the update function
     update(task_data, "titulo4")
 
-    # delete a task that exists in the database
+# Update_state a task with valid data
+def test_update_state_valid_data():
+
+    # Create a mock task_data object
+    task_data = TaskSchema(
+        titulo="titulo5",
+        descripcion="dato para borrar",
+        fecha_vencimiento="2023-10-10",
+        estado="Pendiente"
+    )
+
+    # Call the update function
+    update_state(task_data, "titulo4")
+
+# delete a task that exists in the database
 def test_delete_existing_task():
 
     # Call the delete function with an existing task title
     delete("titulo5")
-
